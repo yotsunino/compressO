d2840ff9efecebf4d6d9048db0c2f52254cd45d0
niraj-khatiwada <nirazzz.nk@gmail.com>
2026-04-05 21:00:03 +0545

    feat: improve website

--- website/src/components/HeroAnimatedContent.tsx
+++ website/src/components/HeroAnimatedContent.tsx
@@ -1,6 +1,9 @@
+import { motion } from 'framer-motion'
+
 import compressoLogo from '../assets/compresso.png'
 import { TextBlurIn } from './effects/TextBlurIn'
 import { TextFade } from './effects/TextFade'
+import TextSlot from './effects/TextSlot'
 import { ZoomInBounce } from './effects/ZoomInBounce'
 
 function HeroAnimatedContent() {
@@ -14,11 +17,26 @@ function HeroAnimatedContent() {
         />
       </ZoomInBounce>
       <TextBlurIn className="badge">✨ 100% Free & Open Source</TextBlurIn>
-      <TextFade direction="down">
-        <h1>
-          Compress any video
-          <br />
-        </h1>
+      <h1 style={{ visibility: 'hidden', fontSize: '0' }}>
+        Compress any video/image
+      </h1>
+      <h1>
+        <motion.span layoutId="item">Compress any </motion.span>
+        <TextSlot
+          texts={['video', 'image']}
+          mainClassName="inline-block md:min-w-[250px] mx-auto text-center flex justify-center"
+          staggerFrom={'first'}
+          initial={{ y: '100%' }}
+          animate={{ y: 0 }}
+          exit={{ y: '-120%' }}
+          staggerDuration={0.025}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+          rotationInterval={2000}
+        />
+        <br />
+      </h1>
+      <TextFade direction="down" className="subtitle">
         <p className="gradient-text title mb-5!">Completely private</p>
       </TextFade>
     </>
